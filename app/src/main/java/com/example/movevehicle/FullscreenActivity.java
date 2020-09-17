6fd3f15d9697c4601fe00842ac626cf417bd537c
6D1996 <43168255+6D1996@users.noreply.github.com>
2020-09-17 14:53:16 +0800

    Update FullscreenActivity.java

--- app/src/main/java/com/example/movevehicle/FullscreenActivity.java
+++ app/src/main/java/com/example/movevehicle/FullscreenActivity.java
@@ -13,6 +13,9 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 
+//I added a line here by Github
+//I added this line by Android Studio
+
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
