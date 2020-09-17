7949b97bfc3e422084a4607ed138db97bd452572
6D1996 <865570543a@gmail.com>
2020-09-17 21:06:47 +0800

    Update FullscreenActivity.java

--- app/src/main/java/com/example/movevehicle/ScalableImageView.java
+++ app/src/main/java/com/example/movevehicle/ScalableImageView.java
@@ -1,6 +1,5 @@
 /**
- * created by jiang, 16/6/14
- * Copyright (c) 2016, jyuesong@gmail.com All Rights Reserved.
+
  * *                #                                                   #
  * #                       _oo0oo_                     #
  * #                      o8888888o                    #
@@ -33,7 +32,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.graphics.PointF;
-import android.os.Vibrator;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -42,10 +40,9 @@ import android.view.animation.RotateAnimation;
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.content.ContentValues.TAG;
 
 /**
- * Created by jiang on 16/6/14.
+ * Fixed on 2020/9/17,which is a happy day!
  */
 public class ScalableImageView extends androidx.appcompat.widget.AppCompatImageView{
 
@@ -58,7 +55,7 @@ public class ScalableImageView extends androidx.appcompat.widget.AppCompatImageV
     private Bitmap bitmap;
     private float rotate = 0F;// 旋转的角度
 
-    List<Float>angleList= new ArrayList<>();
+    List<Float>angleList= new ArrayList<>();//For story the angle data
     int circle =0;
     float degree0=0;//按下角度
     float degree=0;//转动角度
@@ -111,23 +108,11 @@ public class ScalableImageView extends androidx.appcompat.widget.AppCompatImageV
                 mode = Mode.DOWN;
                 mStart.set(event.getX(), event.getY());
                 break;
-/*            case MotionEvent.ACTION_POINTER_DOWN:
-                mPointDistinct = calSpacing(event);
-                if (mPointDistinct > MIN_POINT_DISTINCT) {
-                    cacheMatrix.set(matrix); //先拷贝一份到缓存
-                    calPoint(mEnd, event);
-                    mode = Mode.MOVE;
-                }
-                mDegree = calRotation(event);
-                break;*/
+
             case MotionEvent.ACTION_MOVE:
                 //单点触控的时候
                 if (mode == Mode.DOWN) {
                     matrix.set(cacheMatrix);
-//                    matrix.postTranslate(event.getX() - mStart.x, event.getY() - mStart.y);
-//                    float move = calSpacing(event);
-//                    rotate = calRotation(event);
-//                    float r = rotate - mDegree;
                         double delta_x = (event.getX()- bitmap.getWidth()/2);
                         double delta_y = (event.getY() - bitmap.getHeight()/ 2);
                         double radius = Math.atan2(delta_y,delta_x);
@@ -155,6 +140,7 @@ public class ScalableImageView extends androidx.appcompat.widget.AppCompatImageV
                 break;
             case MotionEvent.ACTION_UP:
                 init();
+                Log.d("Degree",degree+"");
             case MotionEvent.ACTION_POINTER_UP:
             case MotionEvent.ACTION_CANCEL:
                 mode = Mode.NONE;
@@ -164,36 +150,6 @@ public class ScalableImageView extends androidx.appcompat.widget.AppCompatImageV
 
         setImageMatrix(matrix);
         return true;
-    }
-
-
-    private float calSpacing(MotionEvent event) {
-        float x = event.getX(0) - event.getX(1);
-        float y = event.getY(0) - event.getY(1);
-        return (float) Math.sqrt(x * x + y * y);
-    }
-
-    private void calPoint(PointF point, MotionEvent event) {
-        float x = event.getX(0) + event.getX(1);
-        float y = event.getY(0) + event.getY(1);
-        point.set(x / 2, y / 2);
-    }
-
-    private float calRotation(MotionEvent event) {
-        double deltaX = (event.getX(0) - event.getX(1));
-        double deltaY = (event.getY(0) - event.getY(1));
-        double radius = Math.atan2(deltaY, deltaX);
-        return (float) Math.toDegrees(radius);
-    }
-
-
-
-    public void reset() {
-
-        matrix.reset();
-        cacheMatrix.reset();
-        setImageMatrix(matrix);
-        invalidate();
     }
 
 
